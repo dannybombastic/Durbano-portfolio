d68c8b47aff9d1bed61364c06b1182b7d9c1bd64
dannybombastic <dannybombastic@gmail.com>
2023-06-12 19:22:39 +0200

    update skiils

--- main.js
+++ main.js
@@ -48,11 +48,11 @@ window.onscroll = () => {
 
 /*==== scroll reveal ====*/
 
-ScrollReveal({ 
+ScrollReveal({
     //reset: true,
     distance: '80px',
     duration: 2000,
-    delay: 200 
+    delay: 200
 });
 
 ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
@@ -63,7 +63,7 @@ ScrollReveal().reveal('.home-content p, about-content', { origin: 'right' });
 /*==== typed js ====*/
 
 const typed = new Typed('.multiple-text', {
-    strings: ['Web Developer', 'Devopx Developer', 'Full Stack Developer'],
+    strings: ['DevOps Developer', 'CI/CD Developer', 'Cloud Developer','ALM', 'DevSecOps Developer']],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
